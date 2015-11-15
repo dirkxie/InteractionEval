ff22b31ae0f67817ab4593845f0a9a6b21503939
dirkxie <dirkxie@hotmail.com>
2015-11-15 00:22:57 +0000

    update client server code

--- server_client/route.py
+++ server_client/route.py
@@ -1,5 +1,19 @@
 width = 20
 length = 20
+nomRight = 'right'
+nomLeft = 'left'
+nomUp = 'up'
+nomDown = 'down'
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def mapGen(mapTemp):
   for i in xrange (1, length-1):
@@ -13,10 +27,7 @@ def mapGen(mapTemp):
     mapTemp [i][length/2] = ' '
 
 def actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDir):
-  nomRight = 'right'
-  nomLeft = 'left'
-  nomUp = 'up'
-  nomDown = 'down'
+  
 
   if (nomDir == 'right'):
     if (x_prev > x_crnt):
@@ -27,6 +38,8 @@ def actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDir):
       return 'right'
     elif (y_prev < y_crnt):
       return 'left' 
+    else:
+      return 'right'
 
   elif (nomDir == 'left'):
     if (x_prev > x_crnt):
@@ -37,6 +50,8 @@ def actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDir):
       return 'left'
     elif (y_prev < y_crnt):
       return 'right'
+    else:
+      return 'left'
 
   elif (nomDir == 'up'):
     if (x_prev > x_crnt):
@@ -47,6 +62,8 @@ def actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDir):
       return 'forward'
     elif (y_prev < y_crnt):
       return 'backward'
+    else:
+      return 'up'
 
   elif (nomDir == 'down'):
     if (x_prev > x_crnt):
@@ -57,29 +74,44 @@ def actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDir):
       return 'backward'
     elif (y_prev < y_crnt):
       return 'forward'
-
+    else:
+      return 'down'
 
 def navigate(x_prev, y_prev, x_crnt, y_crnt, x_des, y_des, mapTemp):
 
+  print bcolors.OKGREEN + '[PHASE 3] ' + bcolors.ENDC,
+  print 'Previous coordinates:', bcolors.WARNING + str(x_prev), ',', str(y_prev) + bcolors.ENDC
+  
+  print bcolors.OKGREEN + '[PHASE 3] ' + bcolors.ENDC,
+  print 'Current coordinates:', bcolors.WARNING + str(x_crnt), ',', str(y_crnt) + bcolors.ENDC
+  
+  print bcolors.OKGREEN + '[PHASE 3] ' + bcolors.ENDC,
+  print 'Destination:', bcolors.WARNING + str(x_des), ',', str(y_des) + bcolors.ENDC
+  
+  print bcolors.OKGREEN + '[PHASE 4] ' + bcolors.ENDC,
+  print 'Calculating direction'
+ 
+  print bcolors.OKGREEN + '[PHASE 4] ' + bcolors.ENDC,
+
   #destination reached
   if ((x_crnt == x_des) and (y_crnt == y_des)):
-    print 'You are at the destination.\n'
+    print bcolors.FAIL + 'You are at the destination.\n' + bcolors.ENDC
   
-  #turn right on map
-  elif (x_crnt < x_des and mapTemp[x_crnt+1][y_crnt] != '#'):
-    print 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, nomRight)
+  #go down on map
+  elif (y_crnt < y_des) and (mapTemp[x_crnt][y_crnt+1] != '#'):
+    print bcolors.FAIL + 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, 'down') + bcolors.ENDC
 
   #turn left on map
-  elif (x_crnt > x_des and mapTemp[x_crnt-1][y_crnt] != '#'):
-    print 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, nomLeft) 
+  elif (x_crnt > x_des) and (mapTemp[x_crnt-1][y_crnt] != '#'):
+    print bcolors.FAIL + 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, 'left') + bcolors.ENDC 
 
   #go up on map
-  elif (y_crnt > y_des and mapTemp[x_crnt][y_crnt-1] != '#'):
-    print 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, nomUp)
+  elif (y_crnt > y_des) and (mapTemp[x_crnt][y_crnt-1] != '#'):
+    print bcolors.FAIL + 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, 'up') + bcolors.ENDC
 
-  #go down on map
-  elif (y_crnt < y_des and mapTemp[x_crnt][y_crnt+1] != '#'):
-    print 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, nomDown)
+  #turn right on map
+  elif (x_crnt < x_des) and (mapTemp[x_crnt+1][y_crnt] != '#'):
+    print bcolors.FAIL + 'You should move', actualDir(x_prev, y_prev, x_crnt, y_crnt, 'right') + bcolors.ENDC
 
 #def main():
 #  mapTemp = [['#' for i in range (width)] for j in range(length)] 
--- server_client/server.py
+++ server_client/server.py
@@ -1,16 +1,20 @@
 import socket
 import threading
 import time
+import sys
 
 svrSoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Server = '131.179.59.175'
 Server = socket.gethostname()
 print Server
 Port = 567 
+sendDesPort = 9999
+
 svrSoc.bind((Server, Port))
-svrSoc.listen(5)
+svrSoc.listen(10)
 MAC_list = []
 devLoc_list = []
+clnt_IP = '169.232.86.161'
 
 #def test():
 #  while True:
@@ -23,13 +27,25 @@ def ServerSoc():
   while True:
     clntSoc,address = svrSoc.accept()
     print "WE GOT CONNECTION FROM " , address
-    
+   
     #echo back to device
-    clntSoc.send("You are connected\n")
+    clntSoc.send('You are connected to server.')
     
     #receive MAC addr and current location
-    clntMAC = clntSoc.recv(1024)
-    devLoc = clntSoc.recv(1024)
+    clntMAC = clntSoc.recv(100)
+    devLoc = clntSoc.recv(100)
+    clntSoc.send('Location received by server.')
+    opCode = clntSoc.recv(100)
+    
+    #if (opCode == 'reqDes'):
+    print 'opCode = ', opCode
+   
+    #send destination to client
+    clntSoc.send('1')
+    clntSoc.send('18')
+
+    clntSoc.close()
+    
     print 'Device from: ', address, ', MAC address: ', clntMAC
     print 'Device current location: ', devLoc, '\n'
 
@@ -49,23 +65,47 @@ def ServerSoc():
     elif (devLoc != ''):
       devLoc_list[MACIdx].append(devLoc)
 
-    print len(devLoc_list)
+    #print len(devLoc_list)
 
     #print current connected devices
     print 'Current Connected Devices:'
     print MAC_list
+    print 'device location history:'
     print devLoc_list
 
     #update device location
     
-    print 'Device ', clntMAC, 'near ', devLoc
+    #print 'Device ', clntMAC, 'near ', devLoc
     #print clntSoc.recv(1024)
-    clntSoc.close()
+    print '=================================='
 
 def main():
   srvSocThrd = threading.Thread(name='srvSocThrd', target=ServerSoc)
   srvSocThrd.start()
   #testThrd = threading.Thread(name='test', target=test)
   #testThrd.start()
+  
+  '''
+  sendDesSoc = socket.socket()
+  
+  connectSuccess = 0
+  while connectSuccess == 0:
+    connectFlag = 0
 
+    while True:
+    try:
+      sendDesSoc.connect((clnt_IP, sendDesPort))
+    except Exception:
+      print 'failed to connect client'
+      connectFlag = 1
+      sys.exc_clear()
+      time.sleep(2)
+    
+    if (connectFlag == 0):
+      print 'sucessfully connect client, sending destination'
+      connectSuccess = 1
+      sendDesSoc.send('25')
+      sendDesSoc.send('12')
+      sendDesSoc.close()
+  '''
 main()
